694ac29596b072475d1b28430800c85482efa241
Atorte <a@atorte.org>
2025-07-15 00:49:02 +0800

    修复vercel部署失败的bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -149,7 +149,6 @@ export default function Home() {
             <p className="p1">Who Am I?</p>
             <h2>{content.about.title}</h2>
             <p className="p2">{content.about.description}</p>
-            <a href={content.hero.resume} target="_blank"><button>{content.about.button}</button></a>
           </div>
         </div>
       </section>
